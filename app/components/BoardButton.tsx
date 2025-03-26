3450803cd693040a948155e966042f8c64d7d803
goldstargloww <goldstargloww@gmail.com>
2025-03-26 19:15:47 -0400

    the simplest button ever

---
+++ app/components/BoardButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+function BoardButton() {
+    return (
+        <>
+            <button
+                onClick={() => console.log('clicked')}
+            >
+                aaaaaa
+            </button>
+        </>
+    )
+}
+
+export default BoardButton
