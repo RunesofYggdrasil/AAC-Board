f2d58c1fb27e4ea56ad49fe9b7651a7c77a58751
goldstargloww <goldstargloww@gmail.com>
2025-03-29 15:46:31 -0400

    uh i don't remember what i did but i need to switch branches so here

--- app/components/BoardButton.tsx
+++ app/components/BoardButton.tsx
@@ -1,29 +1,34 @@
-'use client';
+"use client";
 
-import styles from './BoardButton.module.css';
+import styles from "./BoardButton.module.css";
+import React, { MouseEvent } from "react";
 
 interface Props {
-    image?: string;
-    label?: string;
-    text: string;
+  image?: string;
+  label?: string;
+  text: string;
 }
 
-function BoardButton({ image = '', label = '', text }: Props) {
-    return (
-        <>
-            <button
-                onClick={() => console.log(text)}
-                className={styles.button}
-            >
-                <div className={styles.buttonImage}>
-                    {image && <img src={image}></img>}
-                </div>
-                <div className={styles.buttonLabel}>
-                    {label}
-                </div>
-            </button>
-        </>
-    )
+const handleClick = (e: MouseEvent<HTMLInputElement>): void => {
+  e.preventDefault();
+};
+
+function BoardButton({ image = "", label = "", text }: Props) {
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => console.log(text)}
+        className={styles.button}
+      >
+        <div
+          className={styles.buttonImage}
+          style={{ backgroundImage: "url(" + image + ")" }}
+        ></div>
+        <div className={styles.buttonLabel}>{label}</div>
+      </button>
+    </>
+  );
 }
 
-export default BoardButton
+export default BoardButton;
